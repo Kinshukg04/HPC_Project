49441383606f292064260c2000028977eb9b0bfe
digishek <45209646+digishek@users.noreply.github.com>
2021-04-16 09:12:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+from getpoints import getpoint
+import client
+import server 
+import multiprocessing
+from multiprocessing import Pool
+import time
+
+x = int(input("1 for server  , 2 for client ")) 
+host=input("input the host of the server") 
+
+port=int(input("input the port"))
+
+
+if(x==1):
+    val=int(input("input the interval")) 
+    server.conn(host, port)
+    server.send(str(val))
+    val = val/2
+    
+    # multiprocessing
+    np = multiprocessing.cpu_count()
+    print ('You have {0:1d} CPUs'.format(np))
+    part_count=[int(val/np) for i in range(np)]
+    pool = Pool(processes=np) 
+    cp = pool.map(getpoint, part_count)
+    cp = sum(cp)
+    print("locally computed",cp)
+    server.send(str(cp)) 
+
+else:
+    val=int(client.recv(host,port).split('.')[0]) 
+    print("interval received" ,val)
+    val = val/2
+    np = multiprocessing.cpu_count()
+    print ('You have {0:1d} CPUs'.format(np))
+    part_count=[int(val/np) for i in range(np)]
+    pool = Pool(processes=np) 
+    cp1 = pool.map(getpoint, part_count)
+    cp1 = sum(cp1)
+    print("locally computed",cp1)
+    cp2=int(client.recv(host,port).split('.')[0])
+    print("received",cp2)
+    pi = getpoint.comp(cp1+cp2,val)
+    print(pi)
+    
+        
+    
+    
---
+++ server.py
@@ -0,0 +1,20 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def conn(host ,port):
+    s.bind((host, port))
+    s.listen(5)
+    
+def send(message):
+   
+    
+    
+    while True:
+        # now our endpoint knows about the OTHER endpoint.
+        clientsocket, address = s.accept()
+        print(f"Connection from {address} has been established.")
+        clientsocket.send(bytes(message,"utf-8"))
+        clientsocket.close()
+        print("sent , now terminating")
+        break
+    
+    
